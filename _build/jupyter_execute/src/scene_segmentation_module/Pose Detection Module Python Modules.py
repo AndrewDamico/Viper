7ad196339c1d16fb7b77c397ce03ea19f0d7035f
Andrew Damico <adamico@ieee.org>
2021-11-28 10:55:20 -0600

    added jupyter notebook files

---
+++ _build/jupyter_execute/src/scene_segmentation_module/Pose Detection Module Python Modules.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Pose Detection Module
+
+# In[1]:
+
+
+import inspect
+
+
+# In[2]:
+
+
+from pose_detection_module import PoseDetectionModel
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
